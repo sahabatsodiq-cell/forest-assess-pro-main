e873fd735a0bcd5add631b7edaf317917ece419b
Syasya Kuliah <syasyakuliah@gmail.com>
2026-08-26 18:03:16 +0800

    fix: resolve vite config syntax error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { intro } from "nitro/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -10,7 +9,6 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
-    nitro()
     tailwindcss(),
     react(),
     tsconfigPaths(),
